6d257e3c2b8b40f9270c0d4cb5721f839cca17f5
orianemgn <mgnoriane@gmail.com>
2022-11-02 16:24:40 +0100

    Step 7 done

--- src/components/categories-list/index.jsx
+++ src/components/categories-list/index.jsx
@@ -1,5 +1,47 @@
+import styles from "./styles.module.css";
+
+const data = [
+    {
+      id: 1,
+      name: "Clothes",
+      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=6011",
+    },
+    {
+      id: 2,
+      name: "Electronics",
+      image: "https://api.lorem.space/image/watch?w=640&h=480&r=304",
+    },
+    {
+      id: 3,
+      name: "Furniture",
+      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=9450",
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=3133",
+    },
+    {
+      id: 5,
+      name: "Others",
+      image: "https://api.lorem.space/image?w=640&h=480&r=4432",
+    },
+  ];
+
+
 export const CategoriesList = () => {
+
     return (
-        <div>Categories List Here</div>
+        <div className={styles.layout}>
+            {data.map((element, index)=>{
+                const {id, name, image} = element; 
+                return (
+                    <div key={id || index}>
+                        <h3>{name}</h3>
+                        <img src={image} alt={name}/>
+                    </div>
+                );
+            })}
+        </div>
     )
 }
