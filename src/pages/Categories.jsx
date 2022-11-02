9df69eca9067b8e5b944581798cf631759c5cf00
orianemgn <mgnoriane@gmail.com>
2022-11-02 15:45:48 +0100

    STEP 1, 2, 3, 4 done

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,17 @@
+const Categories = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <b>file name</b> Categories.jsx
+                </li>
+                <li>
+                    <b>component name</b> Categories
+                </li>
+            </ul>
+        </div>
+        
+    )
+}
+
+export default Categories; 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <b>file name</b> Home.jsx
+                </li>
+                <li>
+                    <b>component name</b> Home
+                </li>
+            </ul>
+        </div>
+    )
+}
+
